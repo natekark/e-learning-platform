5f0a388e75627087ae69e4c53b11e750507bdcb5
natekark <krishnatekar9@gmail.com>
2025-03-23 10:19:00 +0530

    Connecting to MongoDBAtlas

--- server.js
+++ server.js
@@ -28,8 +28,10 @@ app.use(session({
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+  tls: true,
+  tlsAllowInvalidCertificates: false, // Set to true for self-signed certificates (not recommended for production)
+  retryWrites: true,
+  w: 'majority',
 }).then(() => {
   console.log('MongoDB connected...');
 }).catch(err => {
